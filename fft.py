0675873f4ae84d6b8dffdf04c437b8ab09b149a6
Youssef <61395452+Youssef-elkeheil@users.noreply.github.com>
2021-04-13 23:58:51 +0200

    Update fft.py

--- fft.py
+++ fft.py
@@ -1,22 +1,22 @@
 import math
 import numpy as np
 def bands(self):
-        global bands_list, eq_bands
+        global bands, eq_bands
         BW = math.ceil(len(np.abs(yf))/20)
 
         for i in range(0, 20):
-            bands_list.append(yf[i*BW:(i+1)*BW])
+            bands.append(yf[i*BW:(i+1)*BW])
 
         eq_bands.clear()
-        eq_bands = bands_list.copy()
+        eq_bands = bands.copy()
    
 
 def gain(self,i,Gain=1):
     global after_eq
     after_eq.clear()
 
-    eq_bands[i]=bands_list[i]*Gain
-    eq_bands[20-i-1]=bands_list[20-i-1]*Gain
+    eq_bands[i]=bands[i]*Gain
+    eq_bands[20-i-1]=bands[20-i-1]*Gain
     for sublist in eq_bands :
         for x in sublist:
                 after_eq.append(x)
