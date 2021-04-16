297de5df5e81bf81f644405a8a127ab537e1fd13
Youssef <61395452+Youssef-elkeheil@users.noreply.github.com>
2021-04-16 20:44:37 +0200

    equalizer done

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5 import QtMultimedia as M
-from pyqtgraph.widgets.PlotWidget import PlotWidget
 from scipy.fft import rfft, rfftfreq
 from scipy.fft import irfft
 import pyqtgraph as pg
@@ -9,7 +8,6 @@ from scipy import signal
 import RetranslateUI
 import Navigations
 import Resources
-import Equalizer
 import sys
 import numpy as np
 import pandas as pd
@@ -349,8 +347,8 @@ class Ui_MainWindow(object):
         self.actionSlower.triggered.connect(lambda : Navigations.SpeedDown(self))
         
         
-        #for i in range(10):
-        # self.Slider[0].valueChanged.connect(lambda: Gain(self,0,self.Slider[0].value()))
+        for i in range(10):
+            self.Slider[i].valueChanged.connect(lambda: self.fft())
         # self.Slider[1].valueChanged.connect(
         #     lambda: Gain(self,1, self.Slider[1].value()))
         # self.Slider[2].valueChanged.connect(
@@ -433,21 +431,27 @@ class Ui_MainWindow(object):
         self.Spectroplot_After.setLabel('bottom', "Time")
         self.Spectroplot_After.setLabel('left', "Frequency")
 
-    def Gain(self):
-        self.N = self.sampling_rate * self.data/10000
-        normalized_tone = np.int16((self.data / self.data.max()) * 32767)
-        self.yrfft = rfft(normalized_tone)
-        self.xrfft = rfftfreq(self.N, 1 / self.sampling_rate)
-        self.points_per_freq = int(len(self.xrfft) / (self.sampling_rate / 2))
-        self.BW = int(self.points_per_freq*(self.sampling_rate / 20))
-        self.yrfft[:] = 0
-        #slider *self.BW : (slider+1)*self.BW] *= 0
+    def fft(self):
+        self.N = self.sampling_rate * len(self.data)/10000
+        self.yrfft = rfft(self.data)
+        self.xrfft = rfftfreq(int(self.N), 1.0 / self.sampling_rate)
+        print(self.yrfft)
+        print(len(self.yrfft))
+        print(self.xrfft)
+        print(len(self.xrfft))
+        self.BW = int(len(self.yrfft)/10)
+        for i in range (10):
+            self.yrfft[i*self.BW : (i+1)*self.BW]*=self.Slider[i].value()
+            
+
+        # self.yrfft *= self.Slider[0].value()
         self.yt = irfft(self.yrfft)
         self.Graph_After.clear()
-        # self.Graph_After.setTitle('After', color='w', size='12pt')
-        # self.Graph_After.setLabel("left", "Amplitude")
-        # self.Graph_After.setLabel("bottom", "Time")
-        # self.Graph_After.plot(self.yt)
+        self.Graph_After.setTitle('After', color='w', size='12pt')
+        self.Graph_After.setLabel("left", "Amplitude")
+        self.Graph_After.setLabel("bottom", "Time")
+        self.Graph_After.setYRange(-5,5)
+        self.Graph_After.plot(np.real(self.yt))
         # write("Result.wav", self.sampling_rate, self.yt)
 
     
