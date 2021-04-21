edc22f0b6787547cdb6bb0c28a117c42bacc4b81
Youssef <61395452+Youssef-elkeheil@users.noreply.github.com>
2021-04-21 18:10:21 +0200

    Update Navigations.py

--- Navigations.py
+++ Navigations.py
@@ -1,64 +1,59 @@
 
 def Zoom_in(self):
-    self.xrange, yrange = self.Graph_After.viewRange()
-    if self.xrange[1]-self.xrange[0] > self.duration/1000:
-        self.Graph_After.setXRange(self.xrange[0]/2 , self.xrange[1]/2 , padding=0)
-        self.Graph_Before.setXRange(self.xrange[0]/2 , self.xrange[1]/2 , padding=0)
+    xrange, yrange = self.Graph_After.viewRange()
+    if xrange[1]-xrange[0] > self.duration/1000:
+        self.Graph_After.setXRange(xrange[0]/2 , xrange[1]/2 , padding=0)
+        self.Graph_Before.setXRange(xrange[0]/2 , xrange[1]/2 , padding=0)
 
 
 def Zoom_out(self):
-    self.xrange, yrange = self.Graph_After.viewRange()
-    if self.xrange[1]*2 <= self.duration+1:
-        self.Graph_After.setXRange(self.xrange[0]*2 , self.xrange[1] * 2, padding=0)
-        self.Graph_Before.setXRange(self.xrange[0]*2 , self.xrange[1] * 2, padding=0)
+    xrange, yrange = self.Graph_After.viewRange()
+    if xrange[1]*2 <= self.duration+1:
+        self.Graph_After.setXRange(xrange[0]*2 , xrange[1] * 2, padding=0)
+        self.Graph_Before.setXRange(xrange[0]*2 , xrange[1] * 2, padding=0)
     else:
         self.Graph_After.setXRange(0, self.duration, padding=0)
         self.Graph_Before.setXRange(0, len(self.duration), padding=0)
 
 
 def scroll_right(self):
-    self.xrange, yrange = self.Graph_After.viewRange()
-    if self.xrange[1]<self.duration-0.1:
-        self.Graph_After.setXRange(self.xrange[0] + self.speed/1000, self.xrange[1] + self.speed/1000, padding=0)
-        self.Graph_Before.setXRange(
-            self.xrange[0] + self.speed/1000, self.xrange[1] + self.speed/1000, padding=0)
+    xrange, yrange = self.Graph_After.viewRange()
+    if xrange[1] < self.duration-self.speed:
+        self.Graph_After.setXRange(xrange[0] + self.speed, xrange[1] + self.speed, padding=0)
+        self.Graph_Before.setXRange(xrange[0] + self.speed, xrange[1] + self.speed, padding=0)
 
 
 def scroll_left(self):
-    self.xrange, yrange = self.Graph_After.viewRange()
-    if self.xrange[0]>0:
-        self.Graph_After.setXRange(self.xrange[0] - self.speed/1000, self.xrange[1] - self.speed/1000, padding=0)
+    xrange, yrange = self.Graph_After.viewRange()
+    if xrange[0]>0:
+        self.Graph_After.setXRange(xrange[0] - self.speed, xrange[1] - self.speed, padding=0)
         self.Graph_Before.setXRange(
-            self.xrange[0] - self.speed/1000, self.xrange[1] - self.speed/1000, padding=0)
+            xrange[0] - self.speed, xrange[1] - self.speed, padding=0)
     else:
-        self.Graph_After.setXRange(0, self.xrange[1]-self.xrange[0], padding=0)
-        self.Graph_Before.setXRange(0, self.xrange[1]-self.xrange[0], padding=0)
+        self.Graph_After.setXRange(0, xrange[1]-xrange[0], padding=0)
+        self.Graph_Before.setXRange(0, xrange[1]-xrange[0], padding=0)
 
 
 def Update(self):
-    self.xrange, yrange = self.Graph_After.viewRange()
-    if len(self.data) > 0 and self.actionPlay.isChecked():
+    xrange, yrange = self.Graph_After.viewRange()
+    if self.actionPlay.isChecked() and xrange[1]<self.duration:
         self.Graph_After.setXRange(
-            self.xrange[0]+self.speed/10000, self.xrange[1] + self.speed/10000, padding=0)
+            xrange[0]+self.speed/10, xrange[1] + self.speed/10, padding=0)
         self.Graph_Before.setXRange(
-            self.xrange[0]+self.speed/10000, self.xrange[1] + self.speed/10000, padding=0)
-    if self.xrange[1] > self.duration:
+            xrange[0]+self.speed/10, xrange[1] + self.speed/10, padding=0)
+    else:
         self.timer.stop()
         self.actionPlay.setChecked(False)
-        self.Graph_After.setXRange(
-            self.xrange[0]+self.speed/10000, self.duration, padding=0)
-        self.Graph_Before.setXRange(
-            self.xrange[0]+self.speed/10000, self.duration, padding=0)
     
 
 def SpeedUp(self):
-    if self.speed < 1000:
-        self.speed +=100
+    if self.speed < 1:
+        self.speed +=0.25
 
 
 def SpeedDown(self):
-    if self.speed >100:
-        self.speed -= 100
+    if self.speed >0.25:
+        self.speed -= 0.25
 
 
 def ShowEqualizer(self, MainWindow):
